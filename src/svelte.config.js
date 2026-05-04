21f5d2a200f552eb1c6c1bbff6865809f89b35d2
INVIS <juninho.portoj@gmail.com>
2026-05-04 03:54:57 -0300

    svelte.config.js

--- src/svelte.config.js
+++ src/svelte.config.js
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-	<head>
-		<meta charset="utf-8" />
-		<link rel="icon" href="/favicon.png" />
-		<link rel="manifest" href="/manifest.json" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-		<title>INVIS Ecosystem</title>
-		%sveltekit.head%
-	</head>
-	<body style="background-color: #0b0e11; margin: 0; overflow: hidden;">
-		<div style="display: contents">%sveltekit.body%</div>
-	</body>
-</html>
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// O adapter-auto detecta automaticamente que você está na Vercel
+		adapter: adapter()
+	}
+};
