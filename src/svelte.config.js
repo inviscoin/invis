6aff8d9bf48378dd5e6ae7f724181e850d14d146
INVIS <juninho.portoj@gmail.com>
2026-05-04 13:25:31 -0300

    Excluir svelte.config.js src

--- src/svelte.config.js
+++
@@ -1,9 +0,0 @@
-import adapter from '@sveltejs/adapter-auto';
-
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	kit: {
-		// O adapter-auto detecta automaticamente que você está na Vercel
-		adapter: adapter()
-	}
-};
