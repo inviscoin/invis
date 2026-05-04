d97aab7dea58ce8cebb6dd250a979b71140fd0d2
INVIS <juninho.portoj@gmail.com>
2026-05-04 02:54:17 -0300

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'invis-cache-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/manifest.json',
+  '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
