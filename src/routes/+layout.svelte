c820b7399a372c0605b81b7c89a2eae8e47185d0
INVIS <juninho.portoj@gmail.com>
2026-05-07 12:13:44 -0300

    +layout.svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../lib/styles/global.css";
+  import "../lib/styles/tokens.css";
+</script>
+
+<slot />
+
+<style>
+  :global(body) {
+    background-color: var(--bg-main);
+    color: #E0E0E0;
+    font-family: var(--font-main);
+    margin: 0;
+    overflow: hidden;
+  }
+</style>
